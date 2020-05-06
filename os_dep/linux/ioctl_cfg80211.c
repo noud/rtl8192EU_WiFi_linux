b2ff66c58d8b03da98ac336926515414c5bdd8a5
Noud de Brouwer <noud4@home.nl>
2020-05-06 16:53:21 +0200

    linux >= 4.20.x

--- os_dep/linux/ioctl_cfg80211.c
+++ os_dep/linux/ioctl_cfg80211.c
@@ -572,8 +572,13 @@ static const struct ieee80211_txrx_stypes
 static u64 rtw_get_systime_us(void)
 {
 #if (LINUX_VERSION_CODE >= KERNEL_VERSION(2, 6, 39))
+#if (LINUX_VERSION_CODE < KERNEL_VERSION(4, 20, 0))
 	struct timespec ts;
 	get_monotonic_boottime(&ts);
+#else
+	struct timespec64 ts;
+	ktime_get_boottime_ts64(&ts);
+#endif
 	return ((u64)ts.tv_sec * 1000000) + ts.tv_nsec / 1000;
 #else
 	struct timeval tv;
