737658530661cbff1bb3097488ae86cc047868af
Noud de Brouwer <noud4@home.nl>
2020-05-12 10:33:33 +0200

    D-Link DWA-131

--- os_dep/linux/usb_intf.c
+++ os_dep/linux/usb_intf.c
@@ -205,6 +205,8 @@ static struct usb_device_id rtw_usb_id_tbl[] = {
     {USB_DEVICE(0x2357, 0x0107) , .driver_info = RTL8192E}, /* TPLINK -  TL-WN821N */
     {USB_DEVICE(0x2357, 0x0109) , .driver_info = RTL8192E}, /* TPLINK -  TL-WN823N */
     {USB_DEVICE(0x2357, 0x0126) , .driver_info = RTL8192E}, /* TPLINK -  TL-WN8200ND */
+	/*=== Customer ID ===*/
+	{USB_DEVICE(0x2001, 0x3319), .driver_info = RTL8192E}, /* D-Link - ALPHA */
 #endif
 
 #ifdef CONFIG_RTL8723B
